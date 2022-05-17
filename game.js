38185f00bb8cc479ceb36e2159989980abcdbd21
Alexander Bourlotos <alexanderbourlotos@Alexanders-MacBook-Pro.local>
2022-05-17 16:47:17 -0500

    195 - check which button is pressed complete.

--- game.js
+++ game.js
@@ -1,5 +1,7 @@
 var gamePattern = [];
 
+var userClickedPattern = [];
+
 var buttonColors = ["red", "blue", "green", "yellow"];
 
 function newSequence() {
@@ -15,3 +17,9 @@ $("." + randomChosenColor).delay(100).fadeOut(75).fadeIn(75);
 
 var audio = new Audio("sounds/" + randomChosenColor + '.mp3');
 audio.play();
+
+$(".btn").click(function() {
+    var userChosenColor = $(this).attr("id");
+    userClickedPattern.push(userChosenColor);
+    console.log(userClickedPattern);
+});
