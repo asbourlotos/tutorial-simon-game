0d2f9a32660b89ac7e9942729c6d86dcd76f577a
Alex Bourlotos <alex.bourlotos@wnins.com>
2022-05-18 16:23:39 -0500

    restart game function added

--- game.js
+++ game.js
@@ -22,21 +22,26 @@ $(".btn").click(function() {
 
 function checkAnswer(currentLevel) {
     if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
-        console.log("success");
         if (userClickedPattern.length === gamePattern.length) {
             setTimeout(function() {
                 nextSequence();
             }, 1000);
         }
     } else {
-        console.log("wrong");
         playSound("wrong");
         $("body").addClass("game-over");
         setTimeout(function() {
             $("body").removeClass("game-over");
         }, 200);
         $("#level-title").text("Game Over, Press Any Key to Restart");
+        startOver();
     }
+}
+
+function startOver() {
+    level = 0;
+    gamePattern = [];
+    started = false;
 }
 
 function nextSequence() {
